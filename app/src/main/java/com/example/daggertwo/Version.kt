9fa8136899491180a799a99671cbc22b484e75bb
Warrington Bloomfield <warringtonbjr@gmail.com>
2019-05-22 06:55:07 -0400

    Version bump: 1.18.10.0

--- app/src/main/java/com/example/daggertwo/Version.kt
+++ app/src/main/java/com/example/daggertwo/Version.kt
@@ -3,6 +3,6 @@ package com.example.daggertwo
 object Versions {
     const val versionMajor = 1
     const val versionMinor = 18
-    const val versionPatch = 9
+    const val versionPatch = 10
     const val versionBuild = 0
 }
