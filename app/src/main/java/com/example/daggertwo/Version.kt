9b624ab0e4ac2bb4eb3ea68b63df21ef7495db47
Warrington Bloomfield <warringtonbjr@gmail.com>
2019-05-22 06:46:11 -0400

    initial version

---
+++ app/src/main/java/com/example/daggertwo/Version.kt
@@ -0,0 +1,8 @@
+package com.example.daggertwo
+
+object Versions {
+    const val versionMajor = 1
+    const val versionMinor = 18
+    const val versionPatch = 9
+    const val versionBuild = 0
+}
